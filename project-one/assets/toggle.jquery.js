75a366b0121f342964efd6adfa9600761c8dd19e
wlinczer <wlinczer@nd.edu>
2019-09-25 13:37:04 -0400

    09/25/2019 inclass changes

--- project-one/assets/toggle.jquery.js
+++ project-one/assets/toggle.jquery.js
@@ -1,7 +1,31 @@
-$(document).ready(function (){
-    //ID or Class name of the button goes into the first section
-    $('#button').click(function() {
-        //ID or Class name of the modal you want to appear on click goes into this second area.
-        $(".letter-analysis").toggleClass("visible");
-        });
+$(document).ready(function() {
+  //ID or Class name of the button goes into the first section
+  $('#button').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $(".letter-analysis").toggleClass("visible");
+  });
 });
+
+$(document).ready(function() {
+  //ID or Class name of the button goes into the first section
+  $('#button').mouseenter(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#x-mark").addClass("x-op");
+  });
+});
+
+$(document).ready(function() {
+  //ID or Class name of the button goes into the first section
+  $('#button').mouseleave(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#x-mark").removeClass("x-op");
+  });
+});
+
+
+
+
+// $('#button').mouseover(function() {
+//ID or Class name of the modal you want to appear on click goes into this second area.
+// $("#x-mark").toggleClass("x-op");
+// });
