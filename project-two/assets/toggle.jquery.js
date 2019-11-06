9363eb619e51138d08ec7149acee5d6aac4c5ed7
wlinczer <wlinczer@nd.edu>
2019-11-06 12:39:22 -0500

    11/6/2019 in class changes

--- project-two/assets/toggle.jquery.js
+++ project-two/assets/toggle.jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Intro Button
     //ID or Class name of the button goes into the first section
     $('.intro-button-initial').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
@@ -14,6 +15,8 @@ $(document).ready(function () {
           $(".intro-button-initial").removeClass("invisible");
     });
 
+    // Sun
+
     $('.sun-button-initial').click(function () {
         //ID or Class name of the modal you want to appear on click goes into this second area.
         $(".sun-info").removeClass("invisible");
@@ -26,5 +29,181 @@ $(document).ready(function () {
         $(".sun-info").addClass("invisible");
         $(".sun-info").removeClass("visible");
           $(".sun-button-initial").removeClass("invisible");
+    });
+
+    // Mercury
+
+    $('.mercury-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".mercury-info").removeClass("invisible");
+        $(".mercury-info").addClass("visible");
+        $(".mercury-button-initial").addClass("invisible");
+    });
+
+    $('.mercury-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".mercury-info").addClass("invisible");
+        $(".mercury-info").removeClass("visible");
+          $(".mercury-button-initial").removeClass("invisible");
+    });
+
+    // Venus
+
+    $('.venus-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".venus-info").removeClass("invisible");
+        $(".venus-info").addClass("visible");
+        $(".venus-button-initial").addClass("invisible");
+    });
+
+    $('.venus-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".venus-info").addClass("invisible");
+        $(".venus-info").removeClass("visible");
+          $(".venus-button-initial").removeClass("invisible");
+    });
+
+    // Earth
+
+    $('.earth-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".earth-info").removeClass("invisible");
+        $(".earth-info").addClass("visible");
+        $(".earth-button-initial").addClass("invisible");
+    });
+
+    $('.earth-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".earth-info").addClass("invisible");
+        $(".earth-info").removeClass("visible");
+          $(".earth-button-initial").removeClass("invisible");
+    });
+
+    // Mars
+
+    $('.mars-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".mars-info").removeClass("invisible");
+        $(".mars-info").addClass("visible");
+        $(".mars-button-initial").addClass("invisible");
+    });
+
+    $('.mars-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".mars-info").addClass("invisible");
+        $(".mars-info").removeClass("visible");
+          $(".mars-button-initial").removeClass("invisible");
+    });
+
+    // Astroid Belt
+
+    $('.astro-belt-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".astro-belt-info").removeClass("invisible");
+        $(".astro-belt-info").addClass("visible");
+        $(".astro-belt-button-initial").addClass("invisible");
+    });
+
+    $('.astro-belt-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".astro-belt-info").addClass("invisible");
+        $(".astro-belt-info").removeClass("visible");
+          $(".astro-belt-button-initial").removeClass("invisible");
+    });
+
+    // Jupiter
+
+    $('.jupiter-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".jupiter-info").removeClass("invisible");
+        $(".jupiter-info").addClass("visible");
+        $(".jupiter-button-initial").addClass("invisible");
+    });
+
+    $('.jupiter-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".jupiter-info").addClass("invisible");
+        $(".jupiter-info").removeClass("visible");
+          $(".jupiter-button-initial").removeClass("invisible");
+    });
+
+    // Saturn
+
+    $('.saturn-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".saturn-info").removeClass("invisible");
+        $(".saturn-info").addClass("visible");
+        $(".saturn-button-initial").addClass("invisible");
+    });
+
+    $('.saturn-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".saturn-info").addClass("invisible");
+        $(".saturn-info").removeClass("visible");
+          $(".saturn-button-initial").removeClass("invisible");
+    });
+
+    // Uranus
+
+    $('.uranus-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".uranus-info").removeClass("invisible");
+        $(".uranus-info").addClass("visible");
+        $(".uranus-button-initial").addClass("invisible");
+    });
+
+    $('.uranus-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".uranus-info").addClass("invisible");
+        $(".uranus-info").removeClass("visible");
+          $(".uranus-button-initial").removeClass("invisible");
+    });
+
+    // Neptune
+
+    $('.neptune-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".neptune-info").removeClass("invisible");
+        $(".neptune-info").addClass("visible");
+        $(".neptune-button-initial").addClass("invisible");
+    });
+
+    $('.neptune-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".neptune-info").addClass("invisible");
+        $(".neptune-info").removeClass("visible");
+          $(".neptune-button-initial").removeClass("invisible");
+    });
+
+    // Kupier Belt
+
+    $('.kupier-belt-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".kupier-belt-info").removeClass("invisible");
+        $(".kupier-belt-info").addClass("visible");
+        $(".kupier-belt-button-initial").addClass("invisible");
+    });
+
+    $('.kupier-belt-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".kupier-belt-info").addClass("invisible");
+        $(".kupier-belt-info").removeClass("visible");
+          $(".kupier-belt-button-initial").removeClass("invisible");
+    });
+
+    // Pluto
+
+    $('.pluto-button-initial').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".pluto-info").removeClass("invisible");
+        $(".pluto-info").addClass("visible");
+        $(".pluto-button-initial").addClass("invisible");
+    });
+
+    $('.pluto-button-close').click(function () {
+        //ID or Class name of the modal you want to appear on click goes into this second area.
+        $(".pluto-info").addClass("invisible");
+        $(".pluto-info").removeClass("visible");
+          $(".pluto-button-initial").removeClass("invisible");
     });
   });
